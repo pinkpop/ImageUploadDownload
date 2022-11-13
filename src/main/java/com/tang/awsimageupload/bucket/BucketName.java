a687a6c6c71ec407dfeef03906b19741de9797f3
pinkpop <1469137335@qq.com>
2022-11-13 19:44:54 +0900

    first commit

---
+++ src/main/java/com/tang/awsimageupload/bucket/BucketName.java
@@ -0,0 +1,16 @@
+package com.tang.awsimageupload.bucket;
+
+public enum BucketName {
+
+    PROFILE_IMAGE("tang-image-upload-123");
+
+    private final String bucketName;
+
+    BucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+}
