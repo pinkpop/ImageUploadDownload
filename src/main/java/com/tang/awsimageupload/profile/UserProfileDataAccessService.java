0636f496c2e891258079dd520dc2172c78212f08
pinkpop <1469137335@qq.com>
2022-11-21 18:34:16 +0900

    frontend

--- src/main/java/com/tang/awsimageupload/profile/UserProfileDataAccessService.java
+++ src/main/java/com/tang/awsimageupload/profile/UserProfileDataAccessService.java
@@ -1,13 +1,21 @@
 package com.tang.awsimageupload.profile;
 
 import com.tang.awsimageupload.datastore.FakeUserProfileDataStore;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
+
+import static org.apache.http.entity.ContentType.*;
 
 @Repository
 public class UserProfileDataAccessService {
+
+
     private final FakeUserProfileDataStore fakeUserProfileDataStore;
 
     @Autowired
@@ -15,7 +23,10 @@ public class UserProfileDataAccessService {
         this.fakeUserProfileDataStore = fakeUserProfileDataStore;
     }
 
-    List<UserProfile> getUserProfiles() {
+    public List<UserProfile> getUserProfiles() {
+
         return fakeUserProfileDataStore.getUserProfiles();
     }
+
 }
+
